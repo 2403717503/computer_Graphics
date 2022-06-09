abf33705f16603f973ed32b9038e038954c97829
2403717503 <2403717503@qq.com>
2022-06-09 15:02:21 +0000

    add

---
+++ 李静_20201060378/work14/work14.cpp
@@ -0,0 +1,96 @@
+# include <windows.h>
+# include <GL/glut.h>
+# include <stdlib.h>
+# include <math.h>
+
+GLsizei winWidth=600,winHeight=600;
+
+GLfloat xwcMin=-50.0,xwcMax=50.0;
+GLfloat ywcMin=-50.0,ywcMax=50.0;
+
+class wcPt3D{
+public:
+    GLfloat x,y,z;
+};
+
+void init(void){
+    glClearColor(1.0,1.0,1.0,0.0);
+}
+
+void plotPoint(wcPt3D bezCurvePt){
+    glBegin(GL_POINTS);
+    glVertex2f(bezCurvePt.x,bezCurvePt.y);
+    glEnd();
+}
+
+void binomialCoeffs(GLint n,GLint *C){
+    GLint k,j;
+    for(k=0;k<=n;k++){
+        C[k]=1;
+        for(j=n;j>=k+1;j--){
+            C[k]*=j;
+        }
+        for(j=n-k;j>=2;j--){
+            C[k]/=j;
+        }
+    }
+}
+
+void computeBezPt(GLfloat u,wcPt3D *bezPt,GLint nCtr1Pts,wcPt3D *ctr1Pts,GLint *C){
+    GLint k,n=nCtr1Pts-1;
+    GLfloat bezBlendFcn;
+    bezPt->x=bezPt->y=bezPt->z=0.0;
+    for(k=0;k<nCtr1Pts;k++){
+        bezBlendFcn=C[k]*pow(u,k)*pow(1-u,n-k);
+        bezPt->x+=ctr1Pts[k].x*bezBlendFcn;
+        bezPt->y+=ctr1Pts[k].y*bezBlendFcn;
+        bezPt->z+=ctr1Pts[k].z*bezBlendFcn;
+    }
+}
+
+void bezier(wcPt3D *ctr1Pts,GLint nCtr1Pts,GLint nBezCurvePts){
+    wcPt3D bezCurvePt;
+    GLfloat u;
+    GLint *C,k;
+    C=new GLint[nCtr1Pts];
+
+    binomialCoeffs(nCtr1Pts-1,C);
+    for(k=0;k<=nBezCurvePts;k++){
+        u=GLfloat(k)/GLfloat(nBezCurvePts);
+        computeBezPt(u,&bezCurvePt,nCtr1Pts,ctr1Pts,C);
+        plotPoint(bezCurvePt);
+    }
+    delete [ ] C;
+}
+
+void displayFcn(void){
+    GLint nCtr1Pts=4,nBezCurvePts=1000;
+    wcPt3D ctr1Pts[4]={{-40.0,-40.0,0.0},{-10.0,200.0,0.0},{10.0,-200.0,0.0},{40.0,40.0,0.0}};
+    glClear(GL_COLOR_BUFFER_BIT);
+    glPointSize(4);
+    glColor3f(1.0,0.0,0.0);
+    bezier(ctr1Pts,nCtr1Pts,nBezCurvePts);
+    glFlush();
+}
+
+void winReshapeFcn(GLint newWidth,GLint newHeight){
+    glViewport(0,0,newHeight,newHeight);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluOrtho2D(xwcMin,xwcMax,ywcMin,ywcMax);
+    glClear(GL_COLOR_BUFFER_BIT);
+}
+
+int main(int argc,char ** argv){
+    glutInit(&argc,argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowPosition(50,50);
+    glutInitWindowSize(winWidth,winHeight);
+    glutCreateWindow("Bezier Curve");
+
+    init();
+    glutDisplayFunc(displayFcn);
+    glutReshapeFunc(winReshapeFcn);
+    glutMainLoop();
+    return 0;
+}
